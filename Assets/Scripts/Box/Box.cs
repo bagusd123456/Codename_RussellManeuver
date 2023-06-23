0df076e257101d9e6876639c5ccf08886f32f112
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2023-06-24 00:08:14 +0700

    [Bagus] Update Behavior Explosive Box

--- Assets/Scripts/Box/Box.cs
+++ Assets/Scripts/Box/Box.cs
@@ -4,21 +4,57 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    public enum boxState { Idle, Triggered, Blow}
+    public boxState _boxState = boxState.Idle;
     public float radius = 5f;
+    public float blowTime;
+    public float blowCountDown;
+    
+    [Space]
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        blowCountDown = blowTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_boxState == boxState.Idle) return;
+
+        blowCountDown -= Time.deltaTime;
+
+        DisplayCountdown();
+
+        if (_boxState == boxState.Triggered)
+        {
+            if (blowCountDown <= 0f)
+            {
+                Blow();
+            }
+        }
+    }
+
+    public void DisplayCountdown()
+    {
+        int i = Mathf.FloorToInt(blowCountDown);
+        if (i < sprites.Length && i >= 0)
+            spriteRenderer.sprite = sprites[i];
+    }
+
+    public void TriggerBehaviour()
+    {
+        if(_boxState == boxState.Idle)
+            _boxState = boxState.Triggered;
     }
 
     public void Blow()
     {
+        _boxState = boxState.Blow;
+
         Debug.Log("Box " + transform.name + " Blow");
         Collider[] collider = Physics.OverlapSphere(transform.position, radius);
 
@@ -30,10 +66,11 @@ public class Box : MonoBehaviour
                 rb.AddExplosionForce(1000f, transform.position, radius);
             }*/
 
-            if(col.GetComponent<Box>() != null)
+            Box boxTarget;
+            if (col.TryGetComponent(out boxTarget))
             {
-                Box box = col.GetComponent<Box>();
-                box.Blow();
+                if(boxTarget._boxState != boxState.Blow)
+                    boxTarget.Blow();
             }
         }
         Destroy(gameObject);
@@ -42,8 +79,7 @@ public class Box : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
-            Destroy(gameObject);
-            //Blow();
+            TriggerBehaviour();
     }
 
     private void OnDrawGizmos()
